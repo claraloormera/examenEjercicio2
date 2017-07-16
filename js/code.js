9b267c30e668f838befbc3f66cfe952e9d6d17b1
claraloormera <cloormera0203@gmail.com>
2017-07-16 09:41:33 -0500

    ejercicio práctico 2

---
+++ js/code.js
@@ -0,0 +1,23 @@
+/*Se accede al DOM*/
+var parrafo = document.getElementById("texto");
+
+
+
+
+
+var show = function(){
+    
+    
+    /*Con esta instrucción es visible dentro del html*/
+    parrafo.style.display = 'block';
+    
+
+}
+
+/* Cuando el botón sea presionado se llama a la función show*/
+
+button.addEventListener("click", show);
+
+
+
+
